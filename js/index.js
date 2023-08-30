b8c8b82187c45fb761d57481c9575788df097bde
rodrigo-veiga-10 <rodriveiga10@gmail.com>
2023-08-30 19:00:31 +0100

    O teu nome aparece no dashboard
    Novidades para tornar o site com experiência APPLE:
    - Quando entras pela primeira vez o website pergunta o nome
    - Guarda o teu nome e mostra no dashboard
    - Se quiseres alterar só clicar no teu nome :)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 async function fetchTemperature() {
   localStorage.setItem('ip', "192.168.1.199");
   localStorage.setItem("porta", 5000);
+
   try {
+    console.log('Fetching temperature...');
     const response = await fetch('http://' + localStorage.getItem('ip') + ':' + localStorage.getItem('porta') + '/temperatura');
     if (!response.ok) {
       localStorage.setItem('hasError', true);
@@ -11,11 +13,11 @@ async function fetchTemperature() {
     const data = await response.json();
     const temperatura = data.temperatura.toFixed(localStorage.getItem('decimal'));
     console.log('Temperatura:', temperatura);
-    document.querySelector('h1').textContent = 'Temperatura atual: ' + temperatura + ' °C';
+    document.querySelector('.dashboard').textContent = 'Temperatura atual: ' + temperatura + ' °C';
   } catch (error) {
     console.error("Erro ao obter temperatura");
     localStorage.setItem('hasError', true);
-    document.querySelector('h1').textContent = 'Erro';
+    document.querySelector('.dashboard').textContent = 'Erro';
   }
 }
 
@@ -24,14 +26,69 @@ const refreshInterval = storedRefreshInterval ? storedRefreshInterval : 1;
 
 setInterval(fetchTemperature, refreshInterval * 1000);
 
-
-
 function openDialog() {
   var dialog = document.getElementById('versao');
+  console.log('Opening dialog...');
   dialog.showModal();
 }
+
 function closeDialog() {
   var dialog = document.getElementById('versao');
+  console.log('Closing dialog...');
   dialog.close();
 }
+
+window.addEventListener('load', function() {
+  if (localStorage.getItem('promptShown') !== 'true') {
+    alert("Bem vindo ao Servidor PerreVeiga");
+    try {
+      localStorage.setItem('name', prompt("Para teres uma melhor experiência no website, por favor, insere o seu nome:"));
+      localStorage.setItem('promptShown', 'true');
+      alert("Podes sempre mudar o teu nome mais tarde clicando no Bem vindo, o_teu_nome");
+    } catch (error) {
+      console.error('Error occurred while accessing localStorage:', error);
+    }
+  }
+});
+
+window.addEventListener("load", function() {
+  document.getElementById("welcome_name").textContent = "Bem-vindo, " + localStorage.getItem('name');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const h1_name = document.getElementById('welcome_name');
+  if (h1_name) {
+    h1_name.addEventListener('click', function() {
+      console.log("Opening welcome dialog...");
+      try {
+        const dialog = document.getElementById('welcome_dialog');
+        if (dialog) {
+          dialog.showModal();
+        }
+      } catch (error) {
+        console.error("Error displaying the dialog:", error);
+      }
+    });
+  } else {
+    console.warn("Element with class 'container_welcome' not found.");
+  }
+});
+
+function closeDialog_welcome() {
+  var dialog_welcome = document.getElementById('welcome_dialog');
+  console.log('Closing welcome dialog...');
+  dialog_welcome.close();
+}
+
+function changeName() {
+  var nameInput = document.getElementById('name_input');
+  var welcomeName = document.getElementById('welcome_name');
+  if (nameInput.value) {
+    localStorage.setItem('name', nameInput.value);
+    welcomeName.textContent = "Bem-vindo, " + localStorage.getItem('name');
+  }
+}
+
+
+
   
