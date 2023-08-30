b00cca3a76d1b61c576819ff21ea78e6c9175e9f
rodrigo-veiga-10 <rodriveiga10@gmail.com>
2023-08-30 10:42:42 +0100

    Função da temperatura assíncrona
    A função que vai buscar a temperatura agora usa async/await

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-function fetchTemperature() {
-    fetch('http://192.168.1.199:5000/temperatura')
-      .then(response => response.json())
-      .then(data => {
-        const temperatura = data.temperatura.toFixed(localStorage.getItem('decimal'));
-        console.log('Temperatura:', temperatura);
-        document.querySelector('h1').textContent = 'Temperatura atual: ' + temperatura + ' °C';
-      })
-      .catch(error => {
-        console.error('Erro ao obter temperatura:', error);
-        document.querySelector('h1').textContent = 'Erro';
-      });
-  }
+async function fetchTemperature() {
+  try {
+    const response = await fetch('http://192.168.1.199:5000/temperatura');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
 
-  const storedRefreshInterval = localStorage.getItem('refresh');
-  const refreshInterval = storedRefreshInterval ? storedRefreshInterval : 1;
-  setInterval(fetchTemperature, refreshInterval * 1000);
+    const data = await response.json();
+    const temperatura = data.temperatura.toFixed(localStorage.getItem('decimal'));
+    console.log('Temperatura:', temperatura);
+    document.querySelector('h1').textContent = 'Temperatura atual: ' + temperatura + ' °C';
+  } catch (error) {
+    console.error('Erro ao obter temperatura:', error);
+    document.querySelector('h1').textContent = 'Erro';
+  }
+}
+
+const storedRefreshInterval = localStorage.getItem('refresh');
+const refreshInterval = storedRefreshInterval ? storedRefreshInterval : 1;
+
+setInterval(fetchTemperature, refreshInterval * 1000);
+
 
 
 function openDialog() {
