8df5207c8062948ea989eff7c3b0ccdc93669602
rodrigo-veiga-10 <rodriveiga10@gmail.com>
2023-09-01 11:51:20 +0100

    Log de versões
    Agora no botão da versão podes selecionar a versão e ver o que aconteceu nessa versão. Isto é como se fosse a versão 9 e 10 ao mesmo tempo

--- js/index.js
+++ js/index.js
@@ -89,4 +89,31 @@ function changeName() {
   }
 }
 
+
+function showChanges() {
+  const versionSelect = document.getElementById("versionSelect");
+  const selectedVersion = versionSelect.value;
+  const changes = getChangesForVersion(selectedVersion);
+  const changesElement = document.getElementById("changes");
+  changesElement.textContent = `${changes}`;
+}
+
+function getChangesForVersion(version) {
+  const changesMap = {
+    "0.2.10": "Podes selecionar a versão e ver o que aconteceu nessa versão",
+    "0.2.9": "Navbar na parte de baixo do website",
+    "0.2.8": "Nova página FAQ",
+    "0.2.7": "Aparece o teu nome no dasboard",
+    "0.2.6": "Novo estilo do site",
+    "0.2.5": "Informações na página Server",
+    "0.2.4": "Melhorias na função de busca de temperatura no servidor",
+    "0.2.3": "Novo pop-up da versão, o pop-up que tu estás a usar agora",
+    "0.2.2": "Remoção da animação do dasboard",
+    "0.2.1": "Configuração das casas decimais",
+    "0.2": "Primeira configuração - configuração do tempo de atualização da temperatura",
+    "0.1.1": "Mudanças internas do website",
+    "0.1": "Primeira versão do site",
+  };
+  return changesMap[version] || "Nenhuma informação encontrada para essa versão.";
+}
   
