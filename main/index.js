ba657459cac5ed299954ed40f5716f40bfaabcf3
rodrigo-veiga-10 <rodriveiga10@gmail.com>
2023-09-05 13:37:28 +0100

    Mais atualizações de estrutura
    Agora o pop up de versão está no main
    Dá para trocar o nome com a tecla enter

--- main/index.js
+++ main/index.js
@@ -17,6 +17,13 @@ function changeName() {
     welcomeName.textContent = "Bem-vindo, " + localStorage.getItem('name');
   }
 }
+
+var nameInput = document.getElementById('name_input');
+nameInput.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13) {
+    changeName();
+  }
+});
 window.addEventListener("load", function() {
   document.getElementById("welcome_name").textContent = "Bem-vindo, " + localStorage.getItem('name');
 });
@@ -52,3 +59,44 @@ window.addEventListener('load', function() {
     }
   }
 });
+function openDialog() {
+  var dialog = document.getElementById('versao');
+  console.log('Opening dialog...');
+  dialog.showModal();
+}
+
+function closeDialog() {
+  var dialog = document.getElementById('versao');
+  console.log('Closing dialog...');
+  dialog.close();
+}
+function showChanges() {
+  const versionSelect = document.getElementById("versionSelect");
+  const selectedVersion = versionSelect.value;
+  const changes = getChangesForVersion(selectedVersion);
+  const changesElement = document.getElementById("changes");
+  changesElement.textContent = `${changes}`;
+}
+
+function getChangesForVersion(version) {
+  const changesMap = {
+    "3.1": "MEGA ATUALIZAÇÃO na estrutura do website",
+    "3.0": "Website várias funções",
+    "0.2.10.2": "Alterações no estilo do site",
+    "0.2.10.1": "Navbar de cima fica mesmo colada em cima",
+    "0.2.10": "Podes selecionar a versão e ver o que aconteceu nessa versão",
+    "0.2.9": "Navbar na parte de baixo do website",
+    "0.2.8": "Nova página FAQ",
+    "0.2.7": "Aparece o teu nome no dasboard",
+    "0.2.6": "Novo estilo do site",
+    "0.2.5": "Informações na página Server",
+    "0.2.4": "Melhorias na função de busca de temperatura no servidor",
+    "0.2.3": "Novo pop-up da versão, o pop-up que tu estás a usar agora",
+    "0.2.2": "Remoção da animação do dasboard",
+    "0.2.1": "Configuração das casas decimais",
+    "0.2": "Primeira configuração - configuração do tempo de atualização da temperatura",
+    "0.1.1": "Mudanças internas do website",
+    "0.1": "Primeira versão do site",
+  };
+  return changesMap[version] || "Nenhuma informação encontrada para essa versão.";
+}
